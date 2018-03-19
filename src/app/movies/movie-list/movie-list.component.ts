95d1f7380545ce818f9dc7a57bfb990335bf021f
Siddharth Thakor <sidht1295@gmail.com>
2018-03-19 17:32:35 -0400

    movie list and movie detaill updated GUI

--- src/app/movies/movie-list/movie-list.component.ts
+++ src/app/movies/movie-list/movie-list.component.ts
@@ -11,7 +11,13 @@ export class MovieListComponent implements OnInit {
   movies: Movie[] = [
     new Movie('Rush', 'For the Formula 1 lovers',
       'https://cdn.traileraddict.com/content/universal-pictures/rush2013-4.jpg',
-      2013)
+      2013),
+    new Movie('Zindagi Na Milegi Dobara', 'All time Favourite',
+      'https://i1.wp.com/bestoftheyear.in/wp-content/uploads/2016/03/zindagi-na-milegi-dobara.jpg?fit=400%2C578&ssl=1',
+      2011),
+    new Movie('Avatar', 'Revolutionary VFX',
+      'http://t0.gstatic.com/images?q=tbn:ANd9GcQCfmvrE4fMo2cd8esc7mDZPtFSJThAujddMPkRtti1_ij6u-jp',
+      2009)
   ] ;
 
     constructor() { }
